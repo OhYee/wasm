6b2dc8f8ddc42a93b580e525e5953e8fba8dc90e
OhYee <oyohyee@oyohyee.com>
2020-05-13 23:44:27 +0800

    misc: example for go wasm(#master)

---
+++ example/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+
+	"github.com/OhYee/rainbow/errors"
+	"github.com/OhYee/wasm-package/dom"
+	wasm "github.com/OhYee/wasm-package/package"
+)
+
+//go:generate bash -c "GOARCH=wasm GOOS=js go build -o main.wasm main.go"
+func sayHi(name string) string {
+	return fmt.Sprintf("Hi, %s", name)
+}
+
+func main() {
+	pkg := wasm.NewPackage("demo")
+	pkg.ExportFunction("sayHi", func(this js.Value, args []js.Value) (ret interface{}, err error) {
+		if len(args) >= 1 && args[0].Type() == js.TypeString {
+			name := args[0].String()
+			return sayHi(name), nil
+		}
+		err = errors.New("Call failed: sayHi(name:string):string need a string argument")
+		return
+	})
+
+	span := dom.NewSpan()
+	span.SetInnerText("input some thing")
+
+	input := dom.NewInput()
+	input.SetOnInput(
+		func(this dom.Input, args []js.Value) interface{} {
+			span.SetInnerText(this.Value())
+			return nil
+		},
+	)
+
+	dom.Body.AppendChild(input.Element)
+	dom.Body.AppendChild(span.Element)
+
+	pkg.Run()
+}
